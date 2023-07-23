6645b27240dd92b0cfc94da6070147e108e89b04
diyana <labibadiyana100@gmail.com>
2023-07-23 16:47:20 +0600

    top slider done

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../pages/Shared/Navbar/Navbar";
+
+
+const Main = () => {
+    return (
+        <div className="max-w-screen-2xl mx-auto bg-white pt-16">
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Main;
